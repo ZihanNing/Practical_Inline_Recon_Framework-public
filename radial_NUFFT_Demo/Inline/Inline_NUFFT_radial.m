ce7e00dd1e83bb2a3ed83a8bc702b0896ee8b0a7
samr96 <samuel.rot.19@ucl.ac.uk>
2025-07-25 15:51:07 +0200

    mirrored inline_nufft_radial.m

--- radial_NUFFT_Demo/Inline/Inline_NUFFT_radial.m
+++ radial_NUFFT_Demo/Inline/Inline_NUFFT_radial.m
@@ -4,18 +4,12 @@ function [arrayimg1_sos] = Inline_NUFFT_radial(PIPE_folder,twix_like)
 % Main execution script for MERINA sodium MRI k-space to im-
 % age reconstruction. Handles various settings and options, 
 % reconstructs images with optional trajectory shift corr.,
-% combines channel images, saves images and settings. If 
-% applicable, aligns multiple repeats in image space. 
+% (not supported in demo!), combines channel images (sos 
+% only in demo), saves nifti images.
 %-----------------------------------------------------------
 % Sam Rot (UCL)
 %-----------------------------------------------------------
-% Notes: currently only has Finufft recon supported, but pl-
-% anning to include legacy reconstruction for completeness. 
-% Some extensions to come: B1 mapping and correction, B0 map-
-% ping and correction, improved trajectory realignment. 
-%-----------------------------------------------------------
-% This is an inline implementation version PIPE
-% implemented by Zihan Ning (KCL)
+% Notes: reduced functionality for demo
 %===========================================================
 
 %% DEPENDENCIES 
@@ -67,49 +61,14 @@ shift_img = settings.shift_img;
 
 
 %% KSPACE TO IMAGE %%
+% recon of array image(s)
+doshift = 'n'; % trajectory shifting not supported in this demo
 
-%%% recon of volume image %%% 
-if skip_vol_recon == false
-    if shift_img == "Vol" && skip_shift == false
-        doshift = 'y'; 
-    else
-        doshift = 'n';
-    end
-    disp("Commenced recon of volume image")
-    [img_vol, res_vol, ~, shift, shift_TE1] = methods_recon([raw_path,'/',vol_raw_fname],'',Tro_vol,"n","n",[],doshift,shift_noTE,[],[],shift_img,"n",meth_nufft, meth_script);
-    disp("Completed recon of volume image")
-end
+disp("Commenced recon of array image")
+[img_array, res_array, ~, ~, ~] = methods_recon_inline(twix_like,Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
+disp("Completed recon of array image")
 
-%%% recon of array image(s) %%%
-if contains(shift_img,"Arr") && skip_shift == false
-    doshift = 'y'; 
-else
-    doshift = 'n';
-end
-
-if ~(exist('shift', 'var')) % if the shift wasn't calculated from volume images
-    shift = [];
-    shift_TE1 = [];
-end
-
-if no_array_imgs == 2
-    if meth_avg == "Image"
-        disp("Commenced recon of array images")
-        [img_array, res_array, ~, ~, ~] = methods_recon([raw_path,'/',array_raw1_fname],'',Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
-        disp("Reconstructed array image 1/2")
-        img_array2 = methods_recon([raw_path,'/',array_raw2_fname],'',Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
-        disp("Completed recon of array images")
-    else
-        disp("Commenced recon of array images")
-        [img_array, res_array, ~, ~, ~] = methods_recon([raw_path,'/',array_raw1_fname],[raw_path,'/',array_raw2_fname],Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
-    end
-else
-    disp("Commenced recon of array image")
-    [img_array, res_array, ~, ~, ~] = methods_recon_inline(twix_like,Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
-    disp("Completed recon of array image")
-end
-
-%%% sos combination
+%% SOS CHANNEL COMBINATION %% 
 disp("Commenced sos channel combination")
 img_array_sos = squeeze(sqrt(sum(abs(img_array).^2,4)));
 disp("Completed sos channel combination")
