453390cbecd7f5986609dffa86617b5026cce4fb
samr96 <samuel.rot.19@ucl.ac.uk>
2025-07-25 16:13:35 +0200

    removed unused parsed settings  in inline_nufft_radial

--- radial_NUFFT_Demo/Inline/Inline_NUFFT_radial.m
+++ radial_NUFFT_Demo/Inline/Inline_NUFFT_radial.m
@@ -24,34 +24,7 @@ load([PIPE_folder,'/Inline/Config_NUFFT_radial_MEGE.mat']);
 
 %% PARSING INPUT %%
 out_dir = twix_like.hdr.save_path; % save the intermediate result in the same folder as raw
-meth_script = settings.meth_script;
-meth_nufft = settings.meth_nufft;
-finufft_dir = settings.finufft_dir;
-no_array_imgs = settings.no_array_images;
-array_raw1_fname = settings.rawarrname1;
-if no_array_imgs == 2
-    array_raw2_fname = settings.rawarrname2;
-end
-meth_avg = settings.meth_avg;
 Tro = settings.Tro;
-no_TE = settings.no_TE; 
-meth_sense = settings.meth_sense;
-sense_noPointscalib = settings.sense_noPointscalib; 
-path_fsl = settings.path_fsl; 
-
-skip_vol_recon = settings.check_volume;
-vol_raw_fname = settings.rawvolname;
-Tro_vol = settings.Tro;
-no_TE_vol = settings.no_TE; 
-sense_GaussKernel = settings.sense_GaussKernel;
-
-skip_adc = settings.check_adc;
-
-skip_b1_recon = settings.check_b1;
-b1_raw_fname = settings.rawb1name;
-Tro_b1 = settings.Tro;
-no_TE_b1 = settings.no_TE; 
-
 skip_shift = settings.check_shift;
 shift_noTE = settings.shift_noTE;
 shift_img = settings.shift_img; 
@@ -65,7 +38,7 @@ shift_img = settings.shift_img;
 doshift = 'n'; % trajectory shifting not supported in this demo
 
 disp("Commenced recon of array image")
-[img_array, res_array, ~, ~, ~] = methods_recon_inline(twix_like,Tro,"n","n",[],doshift,shift_noTE,shift,shift_TE1,shift_img,"n",meth_nufft, meth_script);
+[img_array, res_array, ~, ~, ~] = methods_recon_inline(twix_like,Tro,"n",doshift,shift_noTE,[],[],shift_img);
 disp("Completed recon of array image")
 
 %% SOS CHANNEL COMBINATION %% 
